01def55c34cd808527dbb3ba7f57390b9007a5a2
Jay Leach <leachcoding@gmail.com>
2021-04-24 18:03:16 -0500

    Euclidean Distance 2.0

--- src/Distance.java
+++ src/Distance.java
@@ -1,12 +1,25 @@
 public class Distance {
     public static void main(String[] args) {
-        // print the Euclidean distance from (x,y) to origin (0,0). This can be represented by sqrt((x-a)^2 + (y-b)^2)
+//        // print the Euclidean distance from (x,y) to origin (0,0). This can be represented by sqrt((x-a)^2 + (y-b)^2)
+//
+//        // x-a squared
+//        int firstEuclidean = (Integer.parseInt(args[0]) - 0) * (Integer.parseInt(args[0]) - 0);
+//
+//        // y-a squared
+//        int secondEuclidean = (Integer.parseInt(args[1]) - 0) * (Integer.parseInt(args[1]) - 0);
+//
+//        // sqrt of the two values
+//        double euclideanDistance = Math.sqrt(firstEuclidean + secondEuclidean);
+//
+//        System.out.println(euclideanDistance);
+
+        // print the Euclidean distance from (x,y) to (a,b). This can be represented by sqrt((x-a)^2 + (y-b)^2)
 
         // x-a squared
-        int firstEuclidean = (Integer.parseInt(args[0]) - 0) * (Integer.parseInt(args[0]) - 0);
+        int firstEuclidean = (Integer.parseInt(args[0]) - Integer.parseInt(args[2])) * (Integer.parseInt(args[0]) - Integer.parseInt(args[2]));
 
         // y-a squared
-        int secondEuclidean = (Integer.parseInt(args[1]) - 0) * (Integer.parseInt(args[1]) - 0);
+        int secondEuclidean = (Integer.parseInt(args[1]) - Integer.parseInt(args[3])) * (Integer.parseInt(args[1]) - Integer.parseInt(args[3]));
 
         // sqrt of the two values
         double euclideanDistance = Math.sqrt(firstEuclidean + secondEuclidean);
