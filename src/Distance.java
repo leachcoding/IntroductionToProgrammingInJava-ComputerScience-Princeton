466e6d2110e7580e4029fae9e5930e59d0302785
Jay Leach <leachcoding@gmail.com>
2021-04-24 18:01:21 -0500

    Euclidean Distance

---
+++ src/Distance.java
@@ -0,0 +1,17 @@
+public class Distance {
+    public static void main(String[] args) {
+        // print the Euclidean distance from (x,y) to origin (0,0). This can be represented by sqrt((x-a)^2 + (y-b)^2)
+
+        // x-a squared
+        int firstEuclidean = (Integer.parseInt(args[0]) - 0) * (Integer.parseInt(args[0]) - 0);
+
+        // y-a squared
+        int secondEuclidean = (Integer.parseInt(args[1]) - 0) * (Integer.parseInt(args[1]) - 0);
+
+        // sqrt of the two values
+        double euclideanDistance = Math.sqrt(firstEuclidean + secondEuclidean);
+
+        System.out.println(euclideanDistance);
+    }
+
+}
